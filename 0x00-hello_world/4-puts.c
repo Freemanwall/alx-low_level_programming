a98e38304c0676ee4c6af6efcef584eeefd4bdad
Freemanwall <sulaimanabdulkareem@yahoo.com>
2022-09-09 07:34:22 -0700

    4-Putsc

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,23 +3,15 @@
 
 
 /**
+ * main - prints the string in the put function
  *
- *  * main - prints the size of various types
- *
- *   *
- *
- *    * Returns: 0 if exited properly, non-zero otherwise
- *
- *     * Return: 0 if exited properly, non-zero otherwise
- *
- *      */
-
+ * Description using the main function
+ * this program prints "programming is like building a multilingual puzzule
+ * Return: 0 
+ */
 int main(void)
-
 {
-
 	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
-
 }
