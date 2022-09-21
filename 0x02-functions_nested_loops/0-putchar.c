f8f50003c6867c820ab4a5b4f4c5741d7b8d6bbf
Freemanwall <sulaimanabdulkareem@yahoo.com>
2022-09-21 00:06:41 -0700

    o putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,20 @@
 #include "main.h"
 
 /**
- * main - print putchar
- * Return: 0 always
+ * main - prints "_putchar"
+ *
+ * Return: 0
  */
+
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	char ch[8] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+		_putchar(ch[i]);
 	_putchar('\n');
+
 	return (0);
-}		
+}
+
