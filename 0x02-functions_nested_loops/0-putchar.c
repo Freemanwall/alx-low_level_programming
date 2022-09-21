342370c5ba5740ac3f25300bb281d1fe376b759a
Freemanwall <sulaimanabdulkareem@yahoo.com>
2022-09-20 18:25:12 -0700

    o-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,7 +4,7 @@
  * main - print putchar
  * Return: always 0
  */
-int main(void)
+ent main(void)
 {
 	_putchar('_');
 	_putchar('p');
@@ -15,5 +15,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
+
 	return (0);
-}
+}		
