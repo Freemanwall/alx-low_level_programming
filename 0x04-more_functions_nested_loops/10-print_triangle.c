2a1958510800cc3e157dd4cff719e9512fa01fcc
Freemanwall <sulaimanabdulkareem@yahoo.com>
2022-09-21 07:25:04 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -32,30 +32,3 @@ void print_triangle(int size)
 	if (i == 1)
 		_putchar('\n');
 }
- 23  
-0x04-more_functions_nested_loops/100-prime_factor.c
-@@ -0,0 +1,23 @@
-#include <stdio.h>
-
-/**
- * main - prime numbers
- *
- * Return: Always 0.
- */
-
-int main(void)
-{
-	long int x = 612852475143;
-	long int py;
-
-	for (py = 2; py < x; py++)
-	{
-		if (x % py == 0)
-		{
-			x = x / py;
-		}
-	}
-	printf("%ld\n", py);
-	return (0);
-}
-
