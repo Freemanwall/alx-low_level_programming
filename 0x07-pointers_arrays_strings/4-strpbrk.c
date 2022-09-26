5eda8fd4f156179f9258e8db8e19f65546c2bf0a
Freemanwall <sulaimanabdulkareem@yahoo.com>
2022-09-26 02:04:14 -0700

    task 4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,18 +1,30 @@
-include "main.h"
+#include "main.h"
 
 /**
- * reverse_array - Reverses the content of an array of integers.
- * @a: The array of integers to be reversed.
- * @n: The number of elements in the array.
+ * _strpbrk - locates the first occurrence in the string s of any
+ * of the bytes in the string accept
+ * @s: for the char
+ * @accept: paramter for accept
+ * Return: pointer to the byte s
  */
-void reverse_array(int *a, int n)
-{
-	int tmp, index;
 
-	for (index = n - 1; index >= n / 2; index--)
+char *_strpbrk(char *s, char *accept)
+{
+	int i, t, j;
+
+	i = 0;
+	while (s[i] != '\0')
 	{
-		tmp = a[n - 1 - index];
-		a[n - 1 - index] = a[index];
-		a[index] = tmp;
+		t = 0;
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+				t = 1;
+		}
+		j = 0;
+		if (t == 1)
+			return (s + i);
+		i++;
 	}
-}	
+	return (0);
+}
